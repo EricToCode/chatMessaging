9304a6465df18239a0c9999a1d85324fb1af7406
Eric Hanyang Chen <ericchen@Erics-MacBook-Pro.local>
2021-06-11 20:46:51 -0400

    Added sgo to home shortcut to pages

--- chatMessaging/urls.py
+++ chatMessaging/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name="home"),
     path('chat/', include('chat.urls')),
 ]
